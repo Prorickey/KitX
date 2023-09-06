cbc422600da5e8a902445d13594176686cd5a420
Trevor Bedson <78997566+Prorickey@users.noreply.github.com>
2023-09-05 21:19:24 -0400

    Records 🤩

--- api/src/main/java/xyz/prorickey/kitx/api/Kit.java
+++ api/src/main/java/xyz/prorickey/kitx/api/Kit.java
@@ -1,27 +1,15 @@
 package xyz.prorickey.kitx.api;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class Kit {
+public record Kit(
+        String name,
+        String permission,
+        Integer cooldown,
+        Integer limit,
+        List<String> items
+) {
 
-    private final String name;
-    private final String permission;
-    private final Integer cooldown;
-    private final Integer limit;
-    private final List<String> items = new ArrayList<>();
 
-    public Kit(String name, String permission, Integer limit, List<String> items, Integer cooldown) {
-        this.name = name;
-        this.permission = permission;
-        this.cooldown = cooldown;
-        this.limit = limit;
-        this.items.addAll(items);
-    }
-    public String getName() { return this.name; }
-    public String getPermission() { return this.permission; }
-    public List<String> getItems() { return this.items; }
-    public Integer getCooldown() { return this.cooldown; }
-    public Integer getLimit() { return this.limit; }
 
 }
